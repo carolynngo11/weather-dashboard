89158f6cce5704ecd485573956a146df010ca7b1
Carolyn Ngo <carolynngo11@yahoo.com>
2022-04-03 23:27:34 -0500

    Added pseudocoding to Java file

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+// Variables
+    // Add city, date, and icon variable
+    // Add temp, wind, humidity, and (uv) index variable
+    // Add forecast card variable
+
+// Click function for search button
+
+// Function for history search button
+
+// Function for weather data
+
+// Function for 5-Day forecast
+
+// Function for 5-Day forecast (date, icon, temp, wind, and humidity)
