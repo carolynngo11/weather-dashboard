9d8b066cc2697ce402a598a7f44bdfe1efc6529c
Carolyn Ngo <carolynngo11@yahoo.com>
2022-04-04 15:12:10 -0500

    Added variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,33 @@
 // Variables
-    // Add submit btn variable
+    // Add API URL
+    // Add search/submit btn variable
     // Add city, date, and icon variable
     // Add temp, wind, humidity, and (uv) index variable
     // Add forecast card variable
 
-var searchBtn = $("historySearchBtn");
+var apiUrl = "";
+
+var searchBtn = $(".historySearchBtn");
+var searchTerm = $("#search-term");
+
+var citynameEl = $(".cityName");
+var currentdateEl = $(".currentDate");
+var weathericonEl = $(".weatherIcon");
+
+var tempEl = $(".temp");
+var windEl = $(".wind");
+var humidityEl = $(".humidity");
+var indexEl = $(".index");
+
+var cardrowEl = $(".card-row")
 
 // Click function for search button
 searchBtn.on("click", function (event) {
     event.preventDefault();
+});
 
 // Function for history search button
+
 
 // Function for weather data
     // UV index color identifier
