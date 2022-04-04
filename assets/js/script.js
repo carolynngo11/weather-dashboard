0b08c05d8a8f82a6504ff7df83d2f196ffe047d7
Carolyn Ngo <carolynngo11@yahoo.com>
2022-04-03 23:42:00 -0500

    Added submit button function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,14 @@
 // Variables
+    // Add submit btn variable
     // Add city, date, and icon variable
     // Add temp, wind, humidity, and (uv) index variable
     // Add forecast card variable
 
+var searchBtn = $("historySearchBtn");
+
 // Click function for search button
+searchBtn.on("click", function (event) {
+    event.preventDefault();
 
 // Function for history search button
 
