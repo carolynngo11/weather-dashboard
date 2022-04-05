7a7cf33cda987408498e989bf8441b361b208eec
Carolyn Ngo <carolynngo11@yahoo.com>
2022-04-04 22:23:25 -0500

    Added function, made HTML edit, removed instructions, added to README

--- README.md
+++ README.md
@@ -1,18 +1,33 @@
 # Weather Dashboard
 
 # Task
-...
+In this assignment I have created a weather dashboard to search a city's weather forecast. The website is expected to display the date, city name, current forecast data, and five-day forecast by using APIs.
 
 # Changed/Edits That Were Made:
 
 **HTML**
-- ...
+- Added CSS and Java links
+    - Bootstrap
+    - Moment.JS
+- Added rows to each section
+    - City search/history
+    - Weather data
+    - Five-day forecast
+- Added classes and id
+- Added button
+- Added search bar
 
 **CSS**
-- ...
+- Header
+    - Added text-allignment, padding, margin, and color gradient
+    - Bold font-weight (to separate headers and city name class)
+- Search section float to the left
+- Added font-weight to weather data class
 
 **Java**
-- ...
+- Added variables
+- Added functions
+- Implemented API URL
 
 IMAGE PLACEHOLDER
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -7,12 +7,13 @@
 
 var apiKey = "46b4cc12b7fab455d566ae2c405fc5fe";
 
-var searchBtn = $(".historySearchBtn");
+var searchBtn = $(".searchBtn");
 var searchTerm = $("#search-term");
 
 var citynameEl = $(".cityName");
 var currentdateEl = $(".currentDate");
 var weathericonEl = $(".weatherIcon");
+var historysearchEl = $(".historySearch")
 
 var tempEl = $(".temp");
 var windEl = $(".wind");
@@ -24,16 +25,22 @@ var cardrowEl = $(".card-row")
 // Click function for search button
 searchBtn.on("click", function (event) {
     event.preventDefault();
+    getWeather(searchTerm.val());
 });
+
+function searchData() {
+    var citySearch = searchTerm.val();
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&appid=" + apiKey;
+    fetch(queryURL)
+    .then(function (response) {
+        return response.json();
+})
 
 // Function for history search button
 
-
-// Function for weather data
+// Function for weather data (city, date, icon variable)
     // UV index color identifier
-moment().format('L');
 
-// Function for 5-Day forecast
-
+// Function for 5-Day forecast cards
 
 // Function for 5-Day forecast (date, icon, temp, wind, and humidity)
